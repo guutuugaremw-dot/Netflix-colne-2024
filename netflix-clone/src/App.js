7a100e87548e58c1c1b79b9e19527242832246ea
guutuugaremw-dot <guutuugaremw@gmail.com>
2026-07-26 16:31:40 -0300

    header and footer

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,11 @@
  import React from 'react';
+import Home from './pages/home/home'; // Changed to match pages/home/home.js
 import './App.css'; 
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Home />
     </div>
   );
 }
---
+++ netflix-clone/src/pages/home/home.js
@@ -0,0 +1,14 @@
+ import React from 'react';
+import Header from "../../components/header/header"; // Capital 'H' for Header, lowercase 'header' for path
+import Footer from "../../components/footer/footer"; // Capital 'F' for Footer, lowercase 'footer' for path
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
