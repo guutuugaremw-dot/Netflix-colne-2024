393293433bbff635d8dae1c6be2651dffd72f81f
guutuugaremw-dot <guutuugaremw@gmail.com>
2026-07-27 04:47:22 -0300

    banner

--- netflix-clone/src/pages/home/home.js
+++ netflix-clone/src/pages/home/home.js
@@ -1,11 +1,13 @@
  import React from 'react';
 import Header from "../../components/header/header"; // Capital 'H' for Header, lowercase 'header' for path
 import Footer from "../../components/footer/footer"; // Capital 'F' for Footer, lowercase 'footer' for path
+import Banner from '../../components/banner/banner';
 
 function Home() {
   return (
     <div>
       <Header />
+      <Banner />
       <Footer />
     </div>
   );
